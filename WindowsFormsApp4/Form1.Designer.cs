a3cec714193f11ead730ef2576e81277b3558a75
Renatasvvk <146754788+Renatasvvk@users.noreply.github.com>
2023-12-22 12:29:28 +0200

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(389, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(636, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ivesti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Atnaujinti";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Kopijuoti visus";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(281, 349);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(233, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Kopijuoti pazymeta";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(25, 406);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(763, 142);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 554);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 30);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Atidaryti";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(175, 554);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 30);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Issaugoti kaip";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(653, 554);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(135, 30);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Valyti";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(529, 159);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 20);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Vidurkis";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(636, 159);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 20);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Mediana";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Vardas Pavarde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(386, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Pazymiai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(633, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Egzamino balas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Esami studentai ir rezultatai";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(386, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Pazymiai ivedami atskiriant tarpais";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 387);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(280, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Laukelis kopijavimui, saugojimui ir pastaboms";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(25, 195);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(763, 148);
+            this.listBox1.TabIndex = 20;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(324, 554);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(124, 30);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Spausdinti";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 596);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string vardasPavarde = textBox1.Text;
+            string pazymiaiString = textBox2.Text;
+            string egzaminoBalasString = textBox3.Text;
+
+           
+            List<double> pazymiai = pazymiaiString.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => !string.IsNullOrWhiteSpace(s)) 
+                .Select(s => Double.Parse(s.Replace(',', '.'))) 
+                .Where(s => s >= 1 && s <= 10) 
+                .ToList();
+
+            
+            double egzaminoBalas;
+            bool egzaminoBalasValid = Double.TryParse(egzaminoBalasString.Replace(',', '.'), out egzaminoBalas) && egzaminoBalas >= 1 && egzaminoBalas <= 10; 
+
+            if (!egzaminoBalasValid || pazymiai.Count == 0)
+            {
+                MessageBox.Show("Neteisingi duomenys ivesti");
+                return;
+            }
+
+            
+            double rezultatas = 0;
+
+            if (radioButton1.Checked) 
+            {
+                if (pazymiai.Count > 0)
+                {
+                    double suma = pazymiai.Sum();
+                    rezultatas = (suma + egzaminoBalas) / (pazymiai.Count + 1);
+                }
+                else
+                {
+                    rezultatas = egzaminoBalas; 
+                }
+            }
+            else if (radioButton2.Checked) 
+            {
+                List<double> pazymiaiKlonas = new List<double>(pazymiai);
+                pazymiaiKlonas.Add(egzaminoBalas);
+                pazymiaiKlonas.Sort();
+
+                int vidurys = pazymiaiKlonas.Count / 2;
+                if (pazymiaiKlonas.Count % 2 == 0)
+                {
+                    rezultatas = (pazymiaiKlonas[vidurys - 1] + pazymiaiKlonas[vidurys]) / 2;
+                }
+                else
+                {
+                    rezultatas = pazymiaiKlonas[vidurys];
+                }
+            }
+
+            
+            string visaInformacija = $"{vardasPavarde}\t {string.Join(" ", pazymiai)}\tEgzamino balas: {egzaminoBalas}\tRezultatas: {rezultatas.ToString("F6")}"; 
+
+            
+            listBox1.Items.Add(visaInformacija);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            listBox1.Items.Clear();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            foreach (object item in listBox1.Items)
+            {
+                textBox5.AppendText(item.ToString() + Environment.NewLine);
+            }
+        }
+        
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                
+                textBox5.AppendText(listBox1.SelectedItem.ToString() + Environment.NewLine);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+          
+            textBox5.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+          
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+          
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+           
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                  
+                    string fileContent = System.IO.File.ReadAllText(openFileDialog.FileName);
+
+                  
+                    textBox5.Text = fileContent;
+                }
+                catch (Exception ex)
+                {
+                    
+                    MessageBox.Show($"Klaida: {ex.Message}");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+           
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    
+                    System.IO.File.WriteAllText(saveFileDialog.FileName, textBox5.Text);
+                    MessageBox.Show("Failas issaugotas");
+                }
+                catch (Exception ex)
+                {
+                   
+                    MessageBox.Show($"Klaida: {ex.Message}");
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+      
+            PrintDialog printDialog = new PrintDialog();
+
+           
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += new PrintPageEventHandler(printDocument_PrintPage);
+
+            printDialog.Document = printDocument;
+
+            
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                  
+                    printDocument.Print();
+                }
+                catch (Exception ex)
+                {
+                    
+                    MessageBox.Show($"Klaida: {ex.Message}");
+                }
+            }
+        }
+        private void printDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+          
+            string textToPrint = textBox5.Text;
+
+            
+            Font printFont = new Font("Arial", 12);
+
+           
+            PointF printLocation = new PointF(10, 10);
+
+           
+            e.Graphics.DrawString(textToPrint, printFont, Brushes.Black, printLocation);
+        }
+
+
+    }
+}
